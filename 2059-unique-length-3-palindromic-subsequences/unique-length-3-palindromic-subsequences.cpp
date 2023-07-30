31945488f1422ab4452f5740c9113bad8c24d387
Pranayagrawal38 <94395772+Pranayagrawal38@users.noreply.github.com>
2023-07-30 11:57:56 +0530

    Time: 294 ms (85.12%) | Memory: 12.9 MB (72.41%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,22 +1,23 @@
-
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<pair<int, int>> v(26, {-1, -1});
-        for (int i = 0; i < s.size(); i++) {
-            if (v[s[i] - 'a'].first == -1) v[s[i] - 'a'].first = i;
-            else v[s[i] - 'a'].second = i;
+    
+        int  n=s.size();
+        vector<int>f(26,1000001),last(26);
+        for(int i=0;i<n;i++){
+            f[s[i]-'a']=min(i,f[s[i]-'a']);
+            last[s[i]-'a']=i;
         }
-        
-        int res = 0;
-        for (int i = 0; i < 26; i++) {
-            if (v[i].second != -1) {
-                unordered_set<char> tmp;
-                for (int j = v[i].first + 1; j < v[i].second; j++) tmp.insert(s[j]);
-                res += tmp.size();
-            }
-        }
-        
-        return res;
+      int  ans=0;
+      for(int i=0;i<26;i++){
+           unordered_set<char>st;
+
+        for(int j=f[i]+1;j<last[i];j++){
+            st.insert(s[j]);
+        } 
+        ans+=st.size();
+  }
+      return ans;
+
     }
 };
