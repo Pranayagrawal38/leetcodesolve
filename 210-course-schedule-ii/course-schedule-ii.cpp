6ef93b3f89ec252f40d720ded9062e9f53d0c460
Pranayagrawal38 <94395772+Pranayagrawal38@users.noreply.github.com>
2023-12-14 16:18:21 +0530

    Time: 11 ms (95.98%) | Memory: 14.2 MB (61.96%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -5,20 +5,13 @@ public:
         for(auto it : prerequisites){
             adj[it[1]].push_back(it[0]);
         }
-        
-        // In-degree array initialization
         vector<int> ind(numCourses, 0);
-        
-        // Calculate in-degrees
         for(int i = 0; i < numCourses; i++){
             for(auto it : adj[i]){
                 ind[it]++;
             }
         }
-        
         queue<int> q;
-        
-        // Add courses with no prerequisites
         for(int i = 0; i < numCourses; i++){
             if(ind[i] == 0){
                 q.push(i);
@@ -31,8 +24,6 @@ public:
             int node = q.front();
             q.pop();
             ans.push_back(node);
-            
-            // Decrease in-degree of neighbors
             for(auto it : adj[node]){
                 ind[it]--;
                 if(ind[it] == 0){
@@ -41,9 +32,8 @@ public:
             }
         }
         
-        // Check if all courses are processed
         if(ans.size() != numCourses) {
-            return {}; // Return empty as we have a cycle or couldn't process all courses
+            return {};
         }
 
        return ans;
